78042c251e0c4aa059c6fd489d04902a4ab5501b
Saurav Kumar <oiiamsaurav@gmail.com>
2026-01-04 23:04:37 +0530

    user Route created and working properely

---
+++ Backend/controllers/orderControllers.js
@@ -0,0 +1,101 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+import Stripe from "stripe";
+import authMiddleware from "../middleware/auth.js";
+
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+//Placing order from frontend;
+const placeOrder = async (req, res) => {
+
+    const frontend_url = "http://localhost:5173" //hardcoded the frontend_url
+
+    try {
+        const newOrder = new orderModel({
+            userId: req.userId,
+            items: req.body.items,
+            amount: req.body.amount,
+            address: req.body.address,
+
+        })
+        await newOrder.save();
+        await userModel.findByIdAndUpdate(req.userId, { cartData: {} });
+
+        const line_items = req.body.items.map((item) => ({
+            price_data: {
+                currency: "inr",
+                product_data: {
+                    name: item.name,
+                },
+                unit_amount: item.price * 100
+                // unit_amount:item.price
+            },
+            quantity: item.quantity
+
+        }))
+        line_items.push({
+            price_data: {
+                currency: "inr",
+                product_data: {
+                    name: "Delivery Charges"
+                },
+                unit_amount: 2 * 100
+            },
+            quantity: 1
+        })
+        const session = await stripe.checkout.sessions.create({
+            line_items: line_items,
+            mode: 'payment',
+            //if payment is successfull redirect to success page else fail page
+            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
+        })
+        res.json({
+            success: true, session_url: session.url
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Order placed failed--inside orderController" });
+
+    }
+
+}
+const verifyOrder = async (req, res) => {
+    const { orderId, success } = req.body;
+    try {
+        if (success == "true") {
+            await orderModel.findByIdAndUpdate(orderId, { payment: true });
+            res.json({
+                success: true,
+                message: "Paid-inside verify Order"
+            })
+        }
+    } catch (error) {
+        await orderModel.findByIdAndDelete(orderId);
+        res.json({ success: false, message: "Not Paid-Inside verify order" });
+        console.log(error);
+    }
+
+}
+
+//userOrder for frontend
+const userOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({ userId: req.userId });
+        res.json({
+            success: true,
+            data: orders
+        });
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message: "Inside userOrder -Error"
+        });
+    }
+};
+
+
+export { placeOrder, verifyOrder, userOrders }
--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ import userRouter from "./routes/userRoute.js";
 
 import 'dotenv/config';
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 
 // appconfig
@@ -23,7 +24,7 @@ app.use("/api/food",foodRouter);
 app.use("/images",express.static('uploads'))
 app.use("/api/users",userRouter);
 app.use("/api/cart",cartRouter);
-
+app.use("/api/order",orderRouter);
 
 // app.use(cors());
 
