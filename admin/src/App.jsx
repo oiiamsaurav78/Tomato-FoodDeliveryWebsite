c0162864ef03ca55b4f43b1d0686e85d73fb9cc9
Saurav Kumar <122017379+oiiamsaurav78@users.noreply.github.com>
2026-01-27 09:58:22 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
-  const url="http://localhost:4000";
+  const url="https://tomato-fooddeliverywebsite-1.onrender.com";
   return (
     <div>
       <ToastContainer/>
