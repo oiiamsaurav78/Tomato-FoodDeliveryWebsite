9c892b9954b65efe8a6094f451b2797619afd134
Saurav Kumar <122017379+oiiamsaurav78@users.noreply.github.com>
2026-01-27 09:57:44 +0530

    Update StoreContext.jsx

--- FrontEnd/src/Contexts/StoreContext.jsx
+++ FrontEnd/src/Contexts/StoreContext.jsx
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://tomato-fooddeliverywebsite-1.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
